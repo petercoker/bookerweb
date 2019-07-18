7e2e4b5664461aebc9054a9596bd635fd9598f7d
peter <petercoker@protonmail.com>
2019-07-18 10:34:41 +0100

    still working on dao file

--- src/com/booker/dao/UserDao.java
+++ src/com/booker/dao/UserDao.java
@@ -59,6 +59,65 @@ public class UserDao {
 	}
 	
 	
+	public void createUser(long id, String email, String password, String firstName, String lastName, GenderEnum gender,
+			UserTypeEnum userType) {
+		User user = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/booker?useSSL=false", "root",
+				"vCEyuK6md6UuoM"); /* a) Database User Profile: root is who the user is b) Database user password */
+				Statement stmt = conn.createStatement();) /* execute mysql queries */ {
+
+//			INSERT INTO User (email, password, first_name, last_name, gender_id, user_type_id, created_date)
+//					VALUES (
+//							"luke.shadow@booker.com", 
+//							'password', 
+//							'Luke',
+//							'Shadow', 
+//							0, 
+//							0, 
+//							NOW()
+//							);
+			
+			String query = "INSERT INTO User (email, password, first_name, last_name, gender_id, user_type_id, created_date)"
+					+ "VALUES ("
+					+ "luke.shadow@booker.com,"
+					+ "'password', "
+					+ "'Luke',"
+					+ "'Shadow', "
+					+ "0, "
+					+ "0, "
+					+ "NOW()"
+					+ ");"
+					"Select * from User where id = " + userId;
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				long id = rs.getLong("id");
+				String email = rs.getString("email");
+				String password = rs.getString("password");
+				String firstName = rs.getString("first_name");
+				String lastName = rs.getString("last_name");
+				int gender_id = rs.getInt("gender_id");
+				GenderEnum gender = GenderEnum.values()[gender_id];
+				int user_type_id = rs.getInt("user_type_id");
+				UserTypeEnum userType = UserTypeEnum.values()[user_type_id];
+				
+				user = UserManager.getInstance().createUser(id, email, password, firstName, lastName, gender, userType);
+	    	}			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		
+		return user;
+		
+	}
+	
+	
 
 	public long authenticate(String email, String encodePassword) {
 		try {
